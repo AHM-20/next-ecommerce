4a6b9f2e8d4e84107340060eddae780343ba947a
Dehmy <abdulrahman.hussein.m@gmail.com>
2022-01-20 06:41:38 +0300

    added useContext

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,35 +1,109 @@
-import React from 'react';
-import { AppBar, Button, Container, Link, Toolbar, Typography } from '@mui/material';
+import React, { useContext } from 'react';
+import {
+	AppBar,
+	Badge,
+	Button,
+	Container,
+	CssBaseline,
+	IconButton,
+	Link,
+	Switch,
+	Toolbar,
+	Typography
+} from '@mui/material';
 import Head from 'next/head';
 import useStyles from '../utils/styles';
 import NextLink from 'next/link';
+// import { ThemeProvider } from '@mui/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Store } from '../utils/Store';
+import { DarkModeSwitch } from './dark-switch';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const Layout = ({ children }) => {
+import Cookies from 'js-cookie';
+
+// import { createTheme } from '@mui/system';
+
+const Layout = ({ title, description, children }) => {
+	const { state, dispatch } = useContext(Store);
+	const { darkMode, cart } = state;
+
+	const theme = createTheme({
+		typography: {
+			h1: {
+				fontSize: '1.6rem',
+				fontWeight: 400,
+				margin: '1rem 0'
+			},
+			h2: {
+				fontSize: '1.4rem',
+				fontWeight: 400,
+				margin: '1rem 0'
+			}
+		},
+		palette: {
+			type: 'dark'
+			// primary: {
+			// 	main: '#07385e'
+			// },
+			// secondary: {
+			// 	main: '#f50057'
+			// }
+		}
+	});
 	const classes = useStyles();
+
+	const changeDarkMode = () => {
+		dispatch({ type: darkMode ? 'DARK_MODE_ON' : 'DARK_MODE_OFF' });
+		const newDarkValue = !darkMode;
+		Cookies.set('darkMode', newDarkValue ? 'ON' : 'OFF');
+	};
+
+
 	return (
 		<div>
 			<Head>
-				<title>Next Commerce</title>
+				<title>{title ? `${title} - Next Commerce` : `Next Commerce`}</title>
+				{description && <meta name="description" content={description} />}
 			</Head>
-			<AppBar position="static" className={classes.navBar}>
-				<Toolbar>
-					<NextLink href={'/'} passHref>
-						<Link>
-							<Typography className={classes.brand}>Next Commerce</Typography>
-						</Link>
-					</NextLink>
-				</Toolbar>
-                <div className={classes.grow}></div>
-                <div className={classes.navActions}>
-                    <NextLink href={'/cart'} passHref><Button>Cart</Button></NextLink>
-                    <NextLink href={'/login'} passHref><Button>Login</Button></NextLink>
-                </div>
-				{/* <Link href={'/'}>Login</Link> */}
-			</AppBar>
-			<Container className={classes.main}>{children}</Container>
-			<footer className={classes.footer}>
-				<Typography>Footer Text</Typography>
-			</footer>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<AppBar position="static" className={classes.navBar}>
+					<Toolbar>
+						<NextLink href={'/'} passHref>
+							<Link>
+								<Typography className={classes.brand}>Next Commerce</Typography>
+							</Link>
+						</NextLink>
+					</Toolbar>
+					<div className={classes.grow} />
+					<div className={classes.navActions}>
+						<Switch checked={darkMode} onChange={changeDarkMode} />
+						<DarkModeSwitch sx={{ m: 1 }} checked={darkMode} onChange={changeDarkMode} />
+						<NextLink href={'/cart'} passHref>
+							{/* <Button>
+								<Badge badgeContent={3} /> Cart
+							</Button> */}
+							<IconButton aria-label="cart">
+								{cart.cartItems.length > 0 ? (
+									<Badge badgeContent={cart.cartItems.length} color="secondary">
+										<ShoppingCartIcon />
+									</Badge>
+								) : (
+									<ShoppingCartIcon />
+								)}
+							</IconButton>
+						</NextLink>
+						<NextLink href={'/login'} passHref>
+							<Button>Login</Button>
+						</NextLink>
+					</div>
+				</AppBar>
+				<Container className={classes.main}>{children}</Container>
+				<footer className={classes.footer}>
+					<Typography>Footer Text</Typography>
+				</footer>
+			</ThemeProvider>
 		</div>
 	);
 };
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,19 +1,126 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import data from '../../utils/data';
+import React, { useContext } from 'react';
+import NextLink from 'next/link';
+import { Link, Grid, List, ListItem, Typography, Rating, Card, Chip, Button } from '@mui/material';
+import Layout from '../../components/Layout';
+import useStyles from '../../utils/styles';
+import Image from 'next/image';
+import db from '../../utils/db';
+import Product from '../../models/Product';
+import axios from 'axios';
+import { Store } from '../../utils/Store';
 
-const ProductScreen = () => {
-	const router = useRouter();
-	const { slug } = router.query;
-	const product = data.products.find((p) => p.slug === slug);
+const ProductScreen = ({ product }) => {
+	const { dispatch } = useContext(Store);
+
+	const classes = useStyles();
+
 	if (!product) {
 		return <h1>Product not found</h1>;
 	}
+
+	const addToCartHandler = async () => {
+		const { data } = await axios.get(`/api/products/${product._id}`);
+		if(data.countInStock <= 0){
+			return window.alert('asdasd')
+		}
+		dispatch({ type: 'CART_ADD', payload: { ...product, quatity: 1 } });
+	};
+
 	return (
-		<div>
-			<h1>{product.name}</h1>
-		</div>
+		<Layout title={product.name} description={product.description}>
+			<div className={classes.section}>
+				<NextLink href="/" passHref>
+					<Link>
+						<Typography>Back to products</Typography>
+					</Link>
+				</NextLink>
+			</div>
+			<Grid container spacing={1}>
+				<Grid item md={6} xs={12}>
+					<Image src={product.image} alt={product.name} width={640} height={640} layout="responsive" />
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<List>
+						<ListItem>
+							<Typography component={'h1'} variant="h1">
+								{product.name}
+							</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Category: {product.category}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Category: {product.category}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Brand: {product.brand}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>Rating: </Typography>
+
+							<Rating name="read-only" value={product.rating} readOnly />
+						</ListItem>
+						<ListItem>
+							<Typography>Description: &nbsp;</Typography>
+							<Typography>{product.description}</Typography>
+						</ListItem>
+					</List>
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<Card>
+						<List>
+							<ListItem>
+								<Grid container>
+									<Grid item>
+										<Typography>Price</Typography>
+									</Grid>
+									<Grid item>
+										<Typography>$ {product.price}</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Grid container>
+									<Grid item>
+										<Typography>Status &nbsp;</Typography>
+									</Grid>
+									<Grid item>
+										<Typography>
+											{product.countInStock > 0 ? (
+												<Chip label="In Stock" variant="outlined" color="primary" />
+											) : (
+												<Chip label="Unavailable" variant="outlined" color="error" />
+											)}
+										</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Button fullWidth variant="contained" color="primary" onClick={addToCartHandler}>
+									Add to cart
+								</Button>
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+			</Grid>
+		</Layout>
 	);
 };
+
+export async function getServerSideProps(context) {
+	const { params } = context;
+	const { slug } = params;
+
+	await db.connect();
+	const product = await Product.findOne({ slug }).lean();
+	await db.disconnect();
+
+	return {
+		props: {
+			product: db.converToObj(product)
+		}
+	};
+}
 
 export default ProductScreen;
